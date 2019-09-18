4ce1660d069f886facb1e4f6613a79919d1662cc
Rahul Gupta <Singlar414@gmail.com>
2019-09-18 23:52:13 +0530

    Create tweentyone.java

---
+++ tweentyone.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution
+{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+	 
+         
+    	while(testCases>0 && in.hasNextLine()){
+			String pattern = in.nextLine();
+          	//Write your codetry
+       try {
+          Pattern p=Pattern.compile(pattern);
+          System.out.println("Valid");
+ }catch(PatternSyntaxException e)       
+ {
+System.out.println("Invalid");
+        }
+      
+        }
+		
+        }
+}
+	
